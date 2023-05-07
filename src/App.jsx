033e21fed537410df8ffc701ae7701ff527793ba
gizzSaw <gizzsaw@gmail.com>
2023-05-07 21:17:25 +0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 import "./styles/main.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Header />
-
-      <main className="section">
-        <div className="container">
-          <ul className="content-list">
-            <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <p>
-                JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM,
-                BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
-              </p>
-            </li>
-            <li className="content-list__item">
-              <h2 className="title-2">Backend</h2>
-              <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-            </li>
-          </ul>
-        </div>
-      </main>
-
+      {/* <Home /> */}
+      {/* <Projects /> */}
+      <Contacts />
       <Footer />
     </div>
   );
