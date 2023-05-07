48ebe10a0bd625c121b9f41f5eb316416e461250
gizzSaw <gizzsaw@gmail.com>
2023-05-07 22:07:14 +0300

    projects.map

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import "./styles/main.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Contacts from "./pages/Contacts";
+import Footer from "./components/Footer/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contacts from "./pages/Contacts.jsx";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* <Home /> */}
-      {/* <Projects /> */}
-      <Contacts />
+      <Projects />
+      {/* <Contacts /> */}
       <Footer />
     </div>
   );
